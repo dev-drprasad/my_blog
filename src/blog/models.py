69a782e58c04f6907ac47790383463c3da8141a0
D REDDY PRASAD <dev.drprasad@aim.com>
2017-07-11 20:37:15 +0530

    added created on, updated on fields

--- src/blog/models.py
+++ src/blog/models.py
@@ -9,6 +9,8 @@ from django.utils.encoding import python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    created_on = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __str__(self):
         return self.title
