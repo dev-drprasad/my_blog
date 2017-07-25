df5b94239f8c40257f78f84293961fb0d027acc7
D REDDY PRASAD <dev.drprasad@aim.com>
2017-07-25 22:08:36 +0530

    :bug: bug fixed in managers

--- src/blog/managers.py
+++ src/blog/managers.py
@@ -21,15 +21,12 @@ class AuthorManager(BaseUserManager):
 
     def create_user(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
-        extra_fields.setdefault('is_staff', False)
+
         return self._create_user(username, email, password, **extra_fields)
 
     def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
